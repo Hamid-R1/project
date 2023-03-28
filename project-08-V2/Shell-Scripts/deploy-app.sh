6996357bddb5047e2ef3dbc3dc79d360b0ad8098
Hamid-R1 <112654341+Hamid-R1@users.noreply.github.com>
2023-03-29 00:00:22 +0530

    Update deploy-app.sh

--- project-08-V2/Shell-Scripts/deploy-app.sh
+++ project-08-V2/Shell-Scripts/deploy-app.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -xe
 
-
 # System Updates
 sudo yum -y update
-
 
 # STEP 1 - Install system software - including Web and DB
 sudo yum install -y mariadb-server httpd
 sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-
 
 # STEP 2 - Web and DB Servers Online - and set to startup
 sudo systemctl start httpd
@@ -16,18 +13,15 @@ sudo systemctl enable httpd
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 
-
 # STEP 3 - Setpassword & DB Variables
 DBName='HR_RDS_DB'
 DBUser='admin'
 DBPassword='Project8'
 DBRootPassword='Admin123root'
-DBEndpoint='copy_here_rds_endpoint'
-
+DBEndpoint='${aws_db_instance.db_instance.address}'
 
 # STEP 4 - Set Mariadb Root Password
 mysqladmin -u root password $DBRootPassword
-
 
 # STEP 5 - Install Wordpress
 sudo wget http://wordpress.org/latest.tar.gz -P /var/www/html
@@ -36,7 +30,6 @@ sudo tar -zxvf latest.tar.gz
 sudo cp -rvf wordpress/* .
 sudo rm -R wordpress
 sudo rm latest.tar.gz
-
 
 # STEP 6 - Configure Wordpress
 sudo cp ./wp-config-sample.php ./wp-config.php
@@ -49,7 +42,6 @@ sudo sed -i "s/'localhost'/'$DBEndpoint'/g" wp-config.php
 sudo usermod -a -G apache ec2-user    #Add your user (in this case, ec2-user) to the apache group. 
 sudo chown -R ec2-user:apache /var/www    #Change the group ownership of /var/www and its contents to the apache group.
 sudo chmod 2775 /var/www
-
 sudo find /var/www -type d -exec chmod 2775 {} \;
 sudo find /var/www -type f -exec chmod 0664 {} \;
 
