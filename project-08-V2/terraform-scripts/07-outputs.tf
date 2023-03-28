4af57d7e82f0a91a7f8766d853b4df13152d9df1
Hamid-R1 <112654341+Hamid-R1@users.noreply.github.com>
2023-03-29 00:07:32 +0530

    Update 07-outputs.tf

--- project-08-V2/terraform-scripts/07-outputs.tf
+++ project-08-V2/terraform-scripts/07-outputs.tf
@@ -1,25 +1,24 @@
 # public-ip of bastion-server
-output "public-ip-bastion-server" {
+output "bastion-server-public-ip" {
   value = aws_instance.wp-bastion-server.public_ip
 }
 
 # private-ip of app-server-1
-output "private-ip-app-server-1" {
+output "app-server-1-private-ip" {
   value = aws_instance.wp-app-server-1.private_ip
 }
 
 # private-ip of app-server-2
-output "private-ip-app-server-2" {
+output "app-server-2-private-ip" {
   value = aws_instance.wp-app-server-2.private_ip
 }
 
 # rds-endpoint
-output "rds_address" {
+output "rds_endpoint" {
   value = aws_db_instance.db_instance.address
 }
 
-# ALB endpoint
-output "dns_name_alb" {
+# ALB dns_name
+output "alb_dns_name" {
   value = aws_lb.wp_alb.dns_name
 }
-
